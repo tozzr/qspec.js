c016d3877f71cbd7534dcf05965940a9957bdf7a
thein <torstenhein@gmx.de>
2013-09-12 00:09:11 +0200

    added an example page

--- qspec.0.1.0.js
+++ qspec.0.1.0.js
@@ -6,77 +6,75 @@
  * licence: MIT
  * 
  */
-var _qspec = {
-	nested: 0,
-	beforeEachFunctions: [],
-	afterEachFunctions: []
-};
-
-
+/*global test, expect, equal, deepEqual, notEqual, notDeepEqual, ok */
 function describe(description, block) {
-	var beforeEachFunctions, afterEachFunctions;
-	
-	beforeEachFunctions = [];
-	afterEachFunctions = [];
-	
-	this.beforeEach = function (beforeEachFunction) {
-		beforeEachFunctions.push(beforeEachFunction);
-	};
-	
-	this.afterEach = function (afterEachFunction) {
-		afterEachFunctions.push(afterEachFunction);
-	};
-	
-	module(description, {
-		setup: function() {
-			for (var i = 0; i < beforeEachFunctions.length; i++) {
-				beforeEachFunctions[i]();			
-			}
-		},
-		teardown: function() {
-			for (var i = 0; i < afterEachFunctions.length; i++) {
-				afterEachFunctions[i]();			
-			}
-		}
-	});	
-	
-	block();
+  var beforeEachFunctions, afterEachFunctions;
+
+  beforeEachFunctions = [];
+  afterEachFunctions = [];
+
+  this.beforeEach = function (beforeEachFunction) {
+    beforeEachFunctions.push(beforeEachFunction);
+  };
+
+  this.afterEach = function (afterEachFunction) {
+    afterEachFunctions.push(afterEachFunction);
+  };
+
+  module(description, {
+    setup: function () {
+      var i;
+      for (i = 0; i < beforeEachFunctions.length; i++) {
+        beforeEachFunctions[i]();
+      }
+    },
+    teardown: function () {
+      var i;
+      for (i = 0; i < afterEachFunctions.length; i++) {
+        afterEachFunctions[i]();
+      }
+    }
+  });
+
+  block();
+}
+
+var xdescribe = function (description, block) {};
+
+var it = test;
+var xit = function (description, block) {};
+
+var expectAssertions = expect;
+
+var expect = function (actual) {
+
+  this.toBe = function (expected) {
+    deepEqual(actual, expected);
+  };
+  this.toBeGreaterThan = function (expected) {
+    ok(actual > expected);
+  };
+  this.toBeNull = function () {
+    deepEqual(actual, null);
+  };
+  this.toEqual = function (expected) {
+    equal(actual, expected);
+  };
+
+  this.not = {
+    toBe: function (expected) {
+      notDeepEqual(actual, expected);
+    },
+    toBeGreaterThan: function (expected) {
+      ok(actual <= expected);
+    },
+    toBeNull: function () {
+      notDeepEqual(actual, null);
+    },
+    toEqual: function (expected) {
+      notEqual(actual, expected);
+    }
+  };
+
+  return this;
 };
-
-xdescribe = function (description, block) {};
-
-it = test;
-xit = function (description, block) {};
-
-expectAssertions = expect;
-
-expect = function (actual) {
-	
-	this.toBe = function (expected) {
-		deepEqual(actual, expected);
-	};
-	this.toBeGreaterThan = function (expected) {
-		ok(actual > expected);
-	};
-	this.toBeNull = function () {
-		deepEqual(actual, null);
-	};
-	this.toEqual = function (expected) {
-		equal(actual, expected);
-	};	
-	
-	this.not = {
-		toBe: function (expected) {
-			notDeepEqual(actual, expected);
-		},
-		toBeNull: function() {
-			notDeepEqual(actual, null);
-		},
-		toEqual: function (expected) {
-			notEqual(actual, expected);
-		}
-	};
-	
-	return this;
-};
-	
