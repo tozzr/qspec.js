eed3d215b928d5226ae30a91a15728c132164fe9
Torsten Hein <hein@webworx.de>
2013-09-13 15:07:11 +0200

    context
    Signed-off-by: Torsten Hein <hein@webworx.de>

--- qspec.0.1.0.js
+++ qspec.0.1.0.js
@@ -25,20 +25,20 @@ function describe(description, block) {
     setup: function () {
       var i;
       for (i = 0; i < beforeEachFunctions.length; i++) {
-        beforeEachFunctions[i]();
+        beforeEachFunctions[i].apply(block);
       }
     },
     teardown: function () {
       var i;
       for (i = 0; i < afterEachFunctions.length; i++) {
-        afterEachFunctions[i]();
+        afterEachFunctions[i].apply(block);
       }
     }
   });
 
   block();
 }
-
+var context = describe;
 var xdescribe = function (description, block) {};
 
 var it = test;
@@ -46,7 +46,7 @@ var xit = function (description, block) {};
 
 var expectAssertions = expect;
 
-var expect = function (actual) {
+function expect( actual) {
 
   this.toBe = function (expected) {
     deepEqual(actual, expected);
@@ -77,4 +77,84 @@ var expect = function (actual) {
   };
 
   return this;
-};
+}
+/**
+ * add some syntactic sugar to qunit
+ * 
+ * load qunit beforehand
+ * @author Torsten Hein <hein@webworx.de>
+ * licence: MIT
+ * 
+ */
+/*global test, expect, equal, deepEqual, notEqual, notDeepEqual, ok */
+function describe(description, block) {
+  var beforeEachFunctions, afterEachFunctions;
+
+  beforeEachFunctions = [];
+  afterEachFunctions = [];
+
+  this.beforeEach = function (beforeEachFunction) {
+    beforeEachFunctions.push(beforeEachFunction);
+  };
+
+  this.afterEach = function (afterEachFunction) {
+    afterEachFunctions.push(afterEachFunction);
+  };
+
+  module(description, {
+    setup: function () {
+      var i;
+      for (i = 0; i < beforeEachFunctions.length; i++) {
+        beforeEachFunctions[i].apply(block);
+      }
+    },
+    teardown: function () {
+      var i;
+      for (i = 0; i < afterEachFunctions.length; i++) {
+        afterEachFunctions[i].apply(block);
+      }
+    }
+  });
+
+  block();
+}
+var context = describe;
+var xdescribe = function (description, block) {};
+
+var it = test;
+var xit = function (description, block) {};
+
+var expectAssertions = expect;
+
+function expect( actual) {
+
+  this.toBe = function (expected) {
+    deepEqual(actual, expected);
+  };
+  this.toBeGreaterThan = function (expected) {
+    ok(actual > expected);
+  };
+  this.toBeNull = function () {
+    deepEqual(actual, null);
+  };
+  this.toEqual = function (expected) {
+    equal(actual, expected);
+  };
+
+  this.not = {
+    toBe: function (expected) {
+      notDeepEqual(actual, expected);
+    },
+    toBeGreaterThan: function (expected) {
+      ok(actual <= expected);
+    },
+    toBeNull: function () {
+      notDeepEqual(actual, null);
+    },
+    toEqual: function (expected) {
+      notEqual(actual, expected);
+    }
+  };
+
+  return this;
+}
